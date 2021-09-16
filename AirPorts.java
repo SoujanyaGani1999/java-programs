1e49011e94a4117dbcf467a45643e77f7c9ce015
SoujanyaGani1999 <soujanyagani.xworkz@gmail.com>
2021-09-16 20:35:55 +0530

    Daily Tasks

---
+++ AirPorts.java
@@ -0,0 +1,16 @@
+class AirPorts
+{
+public static void main(String a[])
+{
+ String domesticAirPorts[] = {"SpiceJet","IndiGo","Air India"};
+ String internationalAirPorts[] = {"Emirates","British Airways","Etihad Airways"};
+for(int i=0; i<domesticAirPorts.length; i++)
+{
+System.out.println(domesticAirPorts[i]);
+}
+for(int i=0; i<internationalAirPorts.length; i++)
+{
+System.out.println(internationalAirPorts[i]);
+}
+}
+}
